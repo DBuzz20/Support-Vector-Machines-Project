3f5fc15374d6bcbff43e0805b978876979f18aff
DBuzz20 <104134994+DBuzz20@users.noreply.github.com>
2024-12-07 19:40:49 +0100

    Q1 grid_search

--- Question_1_AWS/functions_1_AWS.py
+++ Question_1_AWS/functions_1_AWS.py
@@ -2,21 +2,17 @@ import os
 import gzip
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split, KFold
 from cvxopt import matrix, solvers
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.preprocessing import StandardScaler
-import time
 from sklearn.metrics import confusion_matrix,ConfusionMatrixDisplay
 from statistics import mean
+import time
 
 #solvers.options['abstol'] = 1e-15
 #solvers.options['reltol'] = 1e-15
 solvers.options['show_progress'] = False
 
-#hyperparams----------------------------------------------------------------------------
-gamma=2
-C=1
-eps=1e-5
 
 #DATA EXTRACTION------------------------------------------------------------
 def load_mnist(path, kind='train'):
@@ -63,7 +59,7 @@ yLabel7 = y_all_labels[indexLabel7][:1000].astype('float64')
 X_data = np.concatenate((xLabel1, xLabel5), axis=0)
 Y_data = np.concatenate((yLabel1, yLabel5), axis=0)
 
-#DATA adjustment and normalization-----------------------------------------------------
+#functions definition----------------------------------------------------------
 def binary_class(y):
     for i in range(len(y)):
         if y[i]==1:
@@ -72,60 +68,57 @@ def binary_class(y):
             y[i]=-1
     return y
 
-x_train, x_test, y_train, y_test  = train_test_split(X_data, Y_data, test_size = 0.2, random_state = 1895533)
-
-y_train=binary_class(y_train)
-y_test=binary_class(y_test)
-
-#data normalization
-scaler = StandardScaler()
-x_train = scaler.fit_transform(x_train)
-x_test = scaler.transform(x_test)
-
-#functions definition----------------------------------------------------------
 def pol_ker(x1,x2,gamma):
-    K=(np.dot(x1,x2.T)+1)**gamma
+    K=(x1 @ x2.T + 1) ** gamma
     return K
 
 
-def prediction(alfa,x1,x2,Y,gamma,epsilon,C):
-    #RIVEDI
-    return
+def prediction(alfa,x1,x2,y,gamma,C,eps):
+    svList = []
+    #find all the support vectors
+    for sv in range(len(alfa)):
+        if eps < alfa[sv] < C - eps:
+            svList.append(sv)
+    """
+    for sv in svList:
+        #Compute b from complementary conditions
+        b = (1 / len(svList)) * (np.sum((y[sv] - np.dot((alfa * y).T, pol_ker(x1, x2[sv], gamma).reshape(-1, 1)))))
+    """
+    b = (1 / len(svList)) * (np.sum((y[sv] - np.dot((alfa * y).T, pol_ker(x1, x2[sv], gamma).reshape(-1, 1)) for sv in svList)))
+    
+    #Compute prediction
+    Ker = pol_ker(x1, x2, gamma)
+    pred = np.sign(np.dot((alfa * y).T, Ker) + b)
+
+    return pred
 
 
-def init_M(alfa, y_train, epsilon, C, Kernel):
-    P = y_train.shape[0]
+def init_M(alfa, y_train, eps, C, Ker,P):
     Y = y_train*np.eye(P)
-    Q = np.dot(np.dot(Y, Kernel), Y)
+    Q = np.dot(np.dot(Y, Ker), Y)
     
     fun = -(np.dot(Q, alfa) - 1) * y_train  #= - (np.dot(Q, alpha) - np.ones(P).reshape(-1, 1)) * y_train
-    
     S_a = np.where(
         np.logical_or(
-            np.logical_and(alfa < C-epsilon, y_train==-1), np.logical_and(alfa > epsilon ,y_train == 1)))[0]
-    
+            np.logical_and(alfa < C-eps, y_train==-1), np.logical_and(alfa > eps ,y_train == 1)))[0]
     M = np.min(fun[S_a])
-        
+
     return M
 
 
-def init_m(alfa, y_train, epsilon, C, K):
-    P = y_train.shape[0]
+def init_m(alfa, y_train, eps, C, Ker,P):
     Y = y_train*np.eye(P)
-    Q = np.dot(np.dot(Y, K), Y)
+    Q = np.dot(np.dot(Y, Ker), Y)
     
     fun = -(np.dot(Q, alfa) - 1) * y_train  #= - (np.dot(Q, alpha) - np.ones(P).reshape(-1, 1)) * y_train
-    
     R_a = np.where(
         np.logical_or(
-            np.logical_and(alfa < C-epsilon, y_train==1), np.logical_and(alfa > epsilon ,y_train == -1)))[0]
-    
+            np.logical_and(alfa < C-eps, y_train==1), np.logical_and(alfa > eps ,y_train == -1)))[0]
     m = np.max(fun[R_a])
     
     return m
 
-def main(x_train,x_test,y_train,y_test,gamma,C):
-    P = y_train.shape[0]
+def train(x_train,y_train,gamma,C,P):
     Kernel = pol_ker(x_train,x_train,gamma)
     Y_train = y_train * np.eye(P)
     Q_0 = np.dot(np.dot(Y_train, Kernel), Y_train)
@@ -134,42 +127,110 @@ def main(x_train,x_test,y_train,y_test,gamma,C):
     Q = matrix(Q_0)
     e = matrix((-np.ones(P)).reshape(-1, 1))
 
-    #Inequality constraints
+    #Inequality constraints ( Gx <= h )
     G = matrix(np.concatenate((np.eye(P), -np.eye(P)))) #vincoli
-    h = matrix(np.concatenate((C*np.ones((P, 1)), np.zeros((P, 1))))) #termini noti
+    h = matrix(np.concatenate((np.ones((P, 1)) * C, np.zeros((P, 1))))) #termini noti
 
-    #Equality constraints
-    A = matrix(y_train.T)#vincoli #transposed
+    #Equality constraints ( A x = b )
+    A = matrix(y_train.reshape(1, -1))#vincoli #transposed
     b = matrix(np.array([0.])) #termini noti #np.zeros(1)
 
     start = time.time()
     opt = solvers.qp(Q, e, G, h, A, b, solver="cvxopt")
     run_time = time.time() - start
     
-    alfa_star = np.array(opt['x'])
+    alfa_star = np.array(opt['x']) 
+    print("EXITING TRAIN")
+    return alfa_star,run_time,opt,Kernel,Q_0
+    
+
+def printing_routine(x_train,x_test,y_train,y_test,gamma,C,eps,run_time,opt,P,Kernel,Q_0,alfa_star):
+    print(alfa_star)
+    status= opt['status']
+    fun_optimum=opt['primal objective']
     n_it = opt["iterations"]
     fun_opt = (0.5 * (alfa_star.T @ Q_0 @ alfa_star) - np.sum(np.ones(P) * alfa_star))[0][0]
     
-    pred_train = prediction(alfa_star,x_train,x_train,y_train,gamma,eps,C) 
-    acc_train = np.sum(pred_train.ravel() == y_train.ravel())/y_train.size 
+    pred_train = prediction(alfa_star,x_train,x_train,y_train,gamma,eps,C)
+    acc_train = np.sum(pred_train.ravel() == y_train.ravel())/y_train.size()
 
-    pred_test = prediction(alfa_star,x_train,x_test,y_train,gamma,eps,C) 
-    acc_test = np.sum(pred_test.ravel() == y_test.ravel())/y_test.size
+    pred_test = prediction(alfa_star,x_train,x_test,y_train,gamma,eps,C)
+    acc_test = np.sum(pred_test.ravel() == y_test.ravel())/y_test.size()
     
-    M = init_M(alfa_star, y_train, eps, C, Kernel)
-    m = init_m(alfa_star, y_train, eps, C, Kernel) 
+    CM_train = confusion_matrix(y_train.ravel(), pred_train.ravel())
+    CM_test = confusion_matrix(y_test.ravel(), pred_test.ravel())
+    
+    M = init_M(alfa_star, y_train, eps, C, Kernel,P)
+    m = init_m(alfa_star, y_train, eps, C, Kernel,P)
     
     #printing routine
     print("C value: ",C)
     print("Gamma values: ",gamma)
-    
-    print("Accuracy on Training set: ",acc_train)
-    print("Accuracy on test set: ",acc_test)
-    
+    print()
+    print("Accuracy on Training set: %2f" %acc_train)
+    print("Accuracy on test set: %2f" %acc_test)
+    print()
     print("Time spent in optimization: ",run_time)
+    print("Solver status: ",status)
     print("Number of iterations: ",n_it)
-    print("Starting objective function value: 0")
-    print("Optimal objective function value: ",fun_opt)
-    print("KKT violation: ",m-M)
+    #tieni solo uno
+    print("Optimal objective function value: ",fun_opt)   
+    print("Optimal objective function value: ",fun_optimum)
+    print("max KKT violation: ",M-m)
+    
+    TrainCM=ConfusionMatrixDisplay(confusion_matrix=CM_train, display_labels=["T","F"]).plot()
+    TestCM=ConfusionMatrixDisplay(confusion_matrix=CM_test, display_labels=["T","F"]).plot()
+    TrainCM.show()
+    TestCM.show()
     
     return
+
+
+#parametri tipo [C,gamma]-----------------------------------------------------------
+params=[np.array([1,2,3,4,5,10,15,20,25,50,100]),np.arange(2,10,step=1)]
+#-----------------------------------------------------------------------------------
+
+def grid_search(x_train,y_train,eps, params): #avrei usato tutto il db x e y, ma uso x/y_train perche sono gia scalati
+    kf = KFold(n_splits=5, random_state=1895533, shuffle=True)
+    
+    best_acc = float("inf")
+    
+    avg_acc_list=[]
+    
+    for C in params[0]:
+        for gamma in params[1]:
+            acc_train = 0
+            acc_test = 0
+            print("Current hyperparameters => C: ",C,"\tgamma: ",gamma)
+            
+            for train_index, val_index in kf.split(x_train):
+                x_train_fold, x_test_fold = x_train[train_index], x_train[val_index]
+                y_train_fold, y_test_fold = y_train[train_index], y_train[val_index]
+                
+                alfa_star = train(x_train_fold, y_train_fold, gamma, C)[0]
+                
+                pred_train = prediction(alfa_star,x_train_fold,x_train_fold,y_train_fold,gamma,C,eps) 
+                acc_train += np.sum(pred_train.ravel() == y_train_fold.ravel())/y_train_fold.size()
+
+                pred_test = prediction(alfa_star,x_train_fold,x_test_fold,y_train_fold,gamma,C,eps) 
+                acc_test += np.sum(pred_test.ravel() == y_test_fold.ravel())/y_test_fold.size()
+            
+            avg_acc_train = acc_train / kf.get_n_splits()
+            avg_acc_test = acc_test / kf.get_n_splits()
+            
+            avg_acc_list.append([avg_acc_train, avg_acc_test])
+            
+            if avg_acc_test >= best_acc:
+                print("BETTER PARAMS FOUND:")
+                print("C = ",C)
+                print("gamma = ",gamma)
+                best_acc = avg_acc_test
+                best_params = [C, gamma]
+                
+    print("List of average accuracy = ", avg_acc_list)
+    print(best_params) 
+    print(best_acc)
+    
+    return
+            
+            
--- Question_1_AWS/run_1_AWS.py
+++ Question_1_AWS/run_1_AWS.py
@@ -1,3 +1,28 @@
 from functions_1_AWS import *
 
-main(x_train,x_test,y_train,y_test,gamma)
+#hyperparams----------------------------------------------------------------------------
+gamma=2
+C=10
+eps=1e-1
+#DIFFERENZIAZZIONE TRA EPS(PER M E m) E EPS PER CLASSIFIER (?)
+
+#DATA adjustment and normalization-----------------------------------------------------
+x_train, x_test, y_train, y_test  = train_test_split(X_data, Y_data, test_size = 0.2, random_state = 1895533)
+
+y_train=binary_class(y_train)
+y_test=binary_class(y_test)
+
+#normalization
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+#---------------------------------------------------------------------------------------
+P = y_train.shape[0]
+
+alfa_star,run_time,opt,Kernel,Q_0=train(x_train,y_train,gamma,C,P)
+
+printing_routine(x_train,x_test,y_train,y_test,gamma,C,eps,run_time,opt,P,Kernel,Q_0,alfa_star)
+
+"""
+grid_search(x,y,eps,params)
+"""
