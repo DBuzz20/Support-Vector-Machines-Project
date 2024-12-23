f6b0b1d4ec32793a014b1ffc60c543ea17d743a7
DBuzz20 <104134994+DBuzz20@users.noreply.github.com>
2024-12-23 17:32:38 +0100

    Q3 sub-final

--- Question_3_AWS/functions_3_AWS.py
+++ Question_3_AWS/functions_3_AWS.py
@@ -8,12 +8,11 @@ from sklearn.model_selection import train_test_split, KFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix,ConfusionMatrixDisplay
 
-
 gamma=2
 C=1
-q=2
 eps=1e-5
-tol=1e-16
+tol=1e-12
+q=2
 
 def load_mnist(path, kind='train'):
 
@@ -39,6 +38,7 @@ def load_mnist(path, kind='train'):
 cwd = os.getcwd()
 
 X_all_labels, y_all_labels = load_mnist(cwd, kind='train')
+
 """
 We are only interested in the items with label 1, 5 and 7.
 Only a subset of 1000 samples per class will be used.
@@ -59,6 +59,7 @@ x_data=np.concatenate((xLabel1,xLabel5))
 y_data=np.concatenate((yLabel1,yLabel5))
 
 
+
 def binary_class(y):
     for i in range(len(y)):
         if y[i]==1:
@@ -66,153 +67,148 @@ def binary_class(y):
         else:
             y[i]=-1
     return y
-            
+
+
 def pol_ker(x1, x2, gamma):
-    k=(x1 @ x2.T +1)**gamma
+    k=(np.dot(x1,x2.T)+1)**gamma
     return k
 
-def prediction(alfa,x1,x2,y,gamma,C,eps):
-    SV=0 
+
+def prediction(alfa,x1,x2,y,gamma,eps,C):
+    K=pol_ker(x1,x2,gamma)
+    sv=0
     for i in range(len(alfa)):
         if alfa[i]>=eps and alfa[i]<=C-eps:
-            SV+=1
-    if SV==0:
-        print("No SV found")
+            sv+=1
+    if sv==0:
+        print("\n")
+        print("no SV found")
+        print("\n")
         b=0
-    else:      
-        Kb=pol_ker(x1,x1[SV].reshape(1,x1.shape[1]),gamma)
-        b=np.mean(y[SV] - ((alfa*y.reshape(-1,1)).T @ Kb))
-    
-    K=pol_ker(x1,x2,gamma)
-    pred=((alfa*y.reshape(-1,1)).T @ K ) + b
+    else:
+        Kb=pol_ker(x1,x1[sv].reshape(1,x1.shape[1]),gamma)
+        b=np.mean(y[sv]-((alfa*y.reshape(-1,1)).T @ Kb))
+        
+    pred=((alfa*y.reshape(-1,1)).T @ K) + b
     pred=np.sign(pred)
-
     return pred
 
-"""
-def get_M(alfa, y, C,eps,grad, q):
+
+def get_M(alfa, y, eps, C,grad,q):
     grad = grad.reshape((len(alfa), 1))
-    S = np.union1d(np.where((alfa <= C-eps) & (y<0))[0], np.where((alfa >= eps) & (y >0))[0])[0]
-    M_grad=-grad[S]* y[S]
-    M = np.min(M_grad[S])
+    S = np.where(np.logical_or(np.logical_and(alfa <= C-eps, y==-1), np.logical_and(alfa >= eps ,y == 1)))[0]
+    M_grad=-grad[S] * y[S]
+    M = np.min(M_grad)
     
     q2=np.argsort(M_grad.ravel())[0:int(q/2)]
-    
     q2_ind=np.array(S, dtype = int)[q2]
-        
     return M,q2_ind
 
-def get_m(alfa, y, C,eps,grad, q):
+
+def get_m(alfa, y, eps, C,grad,q):
     grad = grad.reshape((len(alfa), 1))
-    R = np.union1d(np.where((alfa <= C-eps) & (y>0))[0], np.where((alfa >= eps) & (y <0))[0])[0]
-    
+    R = np.where(np.logical_or(np.logical_and(alfa <= C-eps, y==1), np.logical_and(alfa >= eps ,y == -1)))[0]
     m_grad = -grad[R] * y[R]
     m = np.max(m_grad)
     
     q1=np.argsort(-m_grad.ravel())[0:int(q/2)]
-    
     q1_ind=np.array(R, dtype = int)[q1]
-    
-    return m,q1_ind
-"""
-
-def get_M(alfa, y, C,eps,grad, q):
-    grad = grad.reshape((len(alfa), 1))
-    indici = np.where(np.logical_or(np.logical_and(alfa <= C-eps, y==-1), np.logical_and(alfa >= eps ,y == 1)))[0]
-    M_grad=-grad[indici] * y[indici]
-    M = np.min(M_grad)
-    q2=np.argsort(M_grad.ravel())[0:int(q/2)]
-    
-    q2_ind=np.array(indici, dtype = int)[q2]
-    
-    
-    return M,q2_ind
-
-def get_m(alfa, y, C,eps,grad, q):
-    grad = grad.reshape((len(alfa), 1))
-    
-    indici = np.where(np.logical_or(np.logical_and(alfa <= C-eps, y==1), np.logical_and(alfa >= eps ,y == -1)))[0]
-    m_grad = -grad[indici] * y[indici]
-    m = np.max(m_grad)
-    
-    q1=np.argsort(-m_grad.ravel())[0:int(q/2)]
-    
-    
-    q1_ind=np.array(indici, dtype = int)[q1]
-    
-    
     return m,q1_ind
 
-def train(x_train,y_train,C,eps,q):
+
+def train(x_train,y_train,gamma,eps,C,q,tol):
     P=y_train.shape[0]
-    alfa=np.zeros(P).reshape(-1,1)
-    gradient=np.ones(P).reshape(-1,1)
-    m, i = get_m(alfa, y_train, C, eps, gradient, q)
-    M, j = get_M(alfa, y_train, C, eps, gradient, q)
-    kernel=pol_ker(x_train,x_train,gamma)
+    y_train=y_train.reshape(P,1)
+    K=pol_ker(x_train,x_train,gamma)
     Y_train=y_train*np.eye(P)
-    Q_0=Y_train@kernel@Y_train
-    n_it=0
     
-    start=time.time()
+    #Q_0=Y_train@K@Y_train
     
-    while m - M >= tol:
-        W_ind=np.concatenate((i,j))
-        Q_tmp=Q_0[W_ind][:,W_ind]
+    alfa=np.zeros((P, 1))
+    grad = -np.ones((P, 1))
+    
+    m, i = get_m(alfa, y_train, eps, C,grad,q)
+    M , j = get_M(alfa, y_train, eps, C,grad,q)
+    
+    buffer={}
+    
+    start = time.time()
+    n_it = 0
+    while (m - M) >= tol:
+ 
+        W_ind = np.concatenate(([i, j]))
+ 
+        if i[0] not in buffer.keys():
+            Ker_m=pol_ker(x_train[i],x_train,gamma)
+            colonna=y_train[i]*(Ker_m @ Y_train)#calcolo colonna
+            
+            buffer['{}'.format(i[0])]=colonna  
+        else:
+            buffer=buffer
+                
+        if j[0] not in buffer.keys():
+            Ker_M=pol_ker(x_train[j],x_train,gamma)
+            colonna=y_train[j]*(Ker_M @ Y_train)#calcolo colonna 
+            buffer['{}'.format(j[0])]=colonna
+        else:
+            buffer=buffer
+      
+        Q_tmp=np.concatenate((buffer['{}'.format(i[0])][0][i],buffer['{}'.format(i[0])][0][j],buffer['{}'.format(j[0])][0][i],buffer['{}'.format(j[0])][0][j])).reshape((2,2))
         
-        #direction
-        fdd=np.array([y_train[i],-y_train[j]]).reshape(-1,1)
+        #feasible descenti direction
+        fdd = np.array([y_train[i], -y_train[j]]).reshape((2,1))
         d_i=fdd[0]
         d_j=fdd[1]
         
         #linesearch
-        t_star=-(gradient[W_ind].T @ fdd)/(fdd.T@Q_tmp@fdd)
-        if d_i>0:
-            max_i=(C-alfa[i])*d_i
-        else:
-            max_i=alfa[i]*np.abs(d_i)
-            
-        if d_j>0:
-            max_j=(C-alfa[j])*d_j
-        else:
-            max_j=alfa[j]*np.abs(d_j)
-            
-        t_max=min(max_i,max_j)
+        t_star=-(grad[W_ind].reshape((1,2)) @ fdd)/(fdd.T@Q_tmp@fdd)
         
-        t_star=min(t_max,t_star)
+        if d_i > 0 :
+            max_i = (C - alfa[i])*d_i
+        else:
+            max_i = alfa[i]*np.abs(d_i)
         
-        #alfa_new only has the W_indexes
-        alfa_new=alfa[W_ind]+t_star*fdd
+        if d_j > 0 :
+            max_j= (C - alfa[i])*d_j
+        else:
+            max_j = alfa[j]*np.abs(d_j)
+        
+        t_max = min(max_i,max_j)
+        t_star =min(t_star,t_max)
+        
+        Q_cols=np.concatenate((buffer['{}'.format(i[0])],buffer['{}'.format(j[0])]))
+        
+        alfa_new=alfa[W_ind] + (t_star*fdd)
         diff=alfa_new-alfa[W_ind]
         
-        gradient=gradient + (Q_0[:,W_ind] @ diff)
-        
+        grad= grad+ (Q_cols.T @ diff)
         alfa[W_ind]=alfa_new
         
-        m, i = get_m(alfa, y_train, C, eps, gradient, q)
-        M , j = get_M(alfa, y_train, C, eps,gradient, q)
+        m, i = get_m(alfa, y_train, eps, C,grad,q)
+        M , j = get_M(alfa, y_train, eps, C,grad,q)
         
         n_it += 1
-        
-    run_time=time.time()-start
+           
+    run_time= time.time() - start
     
     kkt_viol=m-M
-    obj_fun_opt = (0.5 * (alfa.T @ Q_0 @ alfa) - np.sum(np.ones(P) * alfa))[0][0]
     
-    return alfa,run_time,kernel,kkt_viol,n_it,obj_fun_opt
+    return alfa,run_time,n_it,kkt_viol,K
 
-def printing_routine(x_train,x_test,y_train,y_test,gamma,C,eps,run_time,kernel,alfa,kkt_viol,n_it,obj_fun_opt):
+
+def printing_routine(x_train,x_test,y_train,y_test,gamma,eps,C,q,alfa,run_time,n_it,kkt_viol,K):
     P=y_train.shape[0]
+    y_train=y_train.reshape(P,1)
     Y_train=y_train*np.eye(P)
-    pred_train = prediction(alfa,x_train,x_train,y_train,gamma,C,eps) 
+    
+    pred_train = prediction(alfa,x_train,x_train,y_train,gamma,eps,C) 
     acc_train = np.sum(pred_train.ravel() == y_train.ravel())/y_train.size 
 
-    pred_test = prediction(alfa,x_train,x_test,y_train,gamma,C,eps) 
+    pred_test = prediction(alfa,x_train,x_test,y_train,gamma,eps,C) 
     acc_test = np.sum(pred_test.ravel() == y_test.ravel())/y_test.size
     
-    'We calculate Q only to compute the final value of the objective function'
-    Q=np.dot(np.dot(Y_train,kernel),Y_train)
-    FOB=1/2*(np.dot(np.dot(alfa.T,Q),alfa))-np.dot(np.ones((1,len(alfa))),alfa)
+    Q=Y_train @ K @ Y_train #needed for opt_obj_fun
+    opt_obj_fun=(1/2*(alfa.T @ Q @ alfa)-np.ones((1,len(alfa))) @ alfa) [0][0]
     
     print("C value: ",C)
     print("Gamma values: ",gamma)
@@ -223,13 +219,12 @@ def printing_routine(x_train,x_test,y_train,y_test,gamma,C,eps,run_time,kernel,a
     print()
     print("Time spent in optimization: ",run_time)
     print("Number of iterations: ",n_it)
-    print("Optimal objective function value 1: ",obj_fun_opt)
-    print("Optimal objective function value 2: ",FOB)
+    print("Optimal objective function value: ",opt_obj_fun)
     print("max KKT violation: ",kkt_viol)
     
     cm = confusion_matrix(y_test.ravel(), pred_test.ravel()) 
     
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[True,False])
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[1,5])
     disp.plot()
     plt.show()
         
--- Question_3_AWS/run_3_AWS.py
+++ Question_3_AWS/run_3_AWS.py
@@ -1,7 +1,8 @@
 from functions_3_AWS import *
 
+
 #DATA adjustment and normalization-----------------------------------------------------
-x_train, x_test, y_train, y_test  = train_test_split(x_data, y_data, test_size = 0.2, random_state = 1895533)
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size = 0.2, random_state = 1895533)
 
 y_train=binary_class(y_train)
 y_test=binary_class(y_test)
@@ -12,8 +13,6 @@ x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 #---------------------------------------------------------------------------------------
 
-alfa,run_time,kernel,kkt_viol,n_it,obj_fun_opt=train(x_train,y_train,C,eps,q)
+alfa,run_time,n_it,kkt_viol,K=train(x_train,y_train,gamma,eps,C,q,tol)
 
-printing_routine(x_train,x_test,y_train,y_test,gamma,C,eps,run_time,kernel,alfa,kkt_viol,n_it,obj_fun_opt)
-
-#grid_search(x_train, y_train, eps,gamma,C,tol,params)
+printing_routine(x_train,x_test,y_train,y_test,gamma,eps,C,q,alfa,run_time,n_it,kkt_viol,K)
